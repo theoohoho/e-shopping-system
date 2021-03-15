20baa60aac0d1e23cb8d837089e34f371fefff05
theoohoho <topslimer@gmail.com>
2021-03-15 20:46:26 +0800

    Added pydantic model that related with database model

---
+++ flask_app/schemas.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Any, List
+from datetime import datetime
+
+
+class Customer(BaseModel):
+    customer_id: str
+    customer_name: str
+    hashed_password: str
+    email: str
+
+
+class CustomerLoginHistory(BaseModel):
+    id: int
+    customer_id: str
+    login_time: datetime
+    login_status: str
+    ip_address: str
+
+
+class Product(BaseModel):
+    product_id: str
+    product_name: str
+    product_type: Optional[str]
+    store_pcs: int = 0
+    price: int = 0
+    description: Optional[str]
+    release_date: Optional[str]
+    movie_runtime: Optional[str]
+    movie_score: Optional[str]
+    image_url: Optional[str]
+    source_url: Optional[str]
+
+
+class ProductType(BaseModel):
+    id: int
+    type_name: str
+
+
+class Order(BaseModel):
+    order_id: str
+    customer_id: str
+    total_price: int
+    order_time: datetime
+
+
+class OrderItems(BaseModel):
+    id: str
+    order_id: str
+    product_id: str
+    quantity: int
+    price: int
