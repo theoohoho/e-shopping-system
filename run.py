32ca4a7b75bbdeb7ca1045373f958ecaca20d0b0
theoohoho <topslimer@gmail.com>
2021-03-18 09:58:03 +0800

    Modified environ settings for run.py

--- run.py
+++ run.py
@@ -4,4 +4,6 @@ from main import app
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 os.environ['PYTHONPATH'] = BASE_DIR
-app.run(host='localhost', port=8080, debug=True)
+API_PORT = os.environ.get('API_PORT') or 80
+DEBUG_MODE = os.environ.get('DEBUG_MODE') or False
+app.run(host='localhost', port=API_PORT, debug=DEBUG_MODE)
