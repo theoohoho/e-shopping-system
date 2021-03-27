7404068c2058ee19f168c2979c4c30bcd6aa7162
theoohoho <topslimer@gmail.com>
2021-03-27 18:00:15 +0800

    Added get cart items base sql sataement in crud_cart_items.py

--- flask_app/crud/crud_cart_items.py
+++ flask_app/crud/crud_cart_items.py
@@ -12,12 +12,15 @@ from utils.exceptions import crud_exception_handler
 class CRUDCartItems(CRUDBase[CartItems, CartItemsSchema]):
     """Defined cart_items crud method"""
 
+    @staticmethod
+    def get_cart_item_base(db: Session):
+        """Base sql statement to get cart items"""
+        return db.query(CartItems, Product).select_from(CartItems).join(Product, CartItems.product_id == Product.product_id)
+
     @crud_exception_handler
-    def get_cart_item_with_product(self, db: Session) -> List:
-        return db.query(
-            CartItems,
-            Product
-        ).select_from(CartItems).join(Product, CartItems.product_id == Product.product_id).all()
+    def get_cart_item_with_product(self, db: Session, cart_id: str) -> List:
+        """Get cart items"""
+        return self.get_cart_item_base(db).filter(CartItems.cart_id == cart_id).all()
 
 
 cart_items = CRUDCartItems(CartItems)
