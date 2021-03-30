f97550f2664a2409d5be12771ed9b56d66e45693
theoohoho <topslimer@gmail.com>
2021-03-30 21:54:14 +0800

    Modifed run app in run.py

--- flask_app/run.py
+++ flask_app/run.py
@@ -1,7 +1,7 @@
 import os
 from main import app
 
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-os.environ['PYTHONPATH'] = BASE_DIR
-app.run(host='localhost', port=8080, debug=True)
+if __name__ == "__main__":
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    os.environ['PYTHONPATH'] = BASE_DIR
+    app.run(host='localhost', port=8080, debug=True)
